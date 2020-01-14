787af5781c52cff4f3fe987e1e7aa01fed05b066
Boyang <biboyang0@126.com>
2020-01-15 03:09:07 +0800

    no message

---
+++ objc-runtime-CN-master/runtime/objc-sel.mm
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2012 Apple Inc.  All Rights Reserved.
+ * 
+ * @APPLE_LICENSE_HEADER_START@
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
+ * 
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ * 
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
+#if __OBJC2__
+
+#include "objc-private.h"
+#include "objc-cache.h"
+
+#if SUPPORT_PREOPT
+static const objc_selopt_t *builtins = NULL;
+#endif
+
+
+static size_t SelrefCount = 0;
+
+static NXMapTable *namedSelectors;
+
+static SEL search_builtins(const char *key);
+
+
+/***********************************************************************
+* sel_init
+* Initialize selector tables and register selectors used internally.
+**********************************************************************/
+void sel_init(size_t selrefCount)
+{
+    // save this value for later
+    SelrefCount = selrefCount;
+
+#if SUPPORT_PREOPT
+    // 预优化的selector，selectors存放在__TEXT, __objc_opt_ro
+    builtins = preoptimizedSelectors();
+
+    if (PrintPreopt  &&  builtins) {
+        uint32_t occupied = builtins->occupied;
+        uint32_t capacity = builtins->capacity;
+        
+        _objc_inform("PREOPTIMIZATION: using selopt at %p", builtins);
+        _objc_inform("PREOPTIMIZATION: %u selectors", occupied);
+        _objc_inform("PREOPTIMIZATION: %u/%u (%u%%) hash table occupancy",
+                     occupied, capacity,
+                     (unsigned)(occupied/(double)capacity*100));
+        }
+#endif
+
+    // Register selectors used by libobjc
+
+#define s(x) SEL_##x = sel_registerNameNoLock(#x, NO)
+#define t(x,y) SEL_##y = sel_registerNameNoLock(#x, NO)
+
+    mutex_locker_t lock(selLock);
+
+    s(load);
+    s(initialize);
+    t(resolveInstanceMethod:, resolveInstanceMethod);
+    t(resolveClassMethod:, resolveClassMethod);
+    t(.cxx_construct, cxx_construct);
+    t(.cxx_destruct, cxx_destruct);
+    s(retain);
+    s(release);
+    s(autorelease);
+    s(retainCount);
+    s(alloc);
+    t(allocWithZone:, allocWithZone);
+    s(dealloc);
+    s(copy);
+    s(new);
+    t(forwardInvocation:, forwardInvocation);
+    t(_tryRetain, tryRetain);
+    t(_isDeallocating, isDeallocating);
+    s(retainWeakReference);
+    s(allowsWeakReference);
+
+#undef s
+#undef t
+}
+
+
+// 创建SEL，从这里，我们可以看到，sel其实就是一个char *字符串
+static SEL sel_alloc(const char *name, bool copy)
+{
+    selLock.assertLocked();
+    return (SEL)(copy ? strdupIfMutable(name) : name);    
+}
+
+
+const char *sel_getName(SEL sel) 
+{
+    if (!sel) return "<null selector>";
+    return (const char *)(const void*)sel;
+}
+
+
+BOOL sel_isMapped(SEL sel) 
+{
+    if (!sel) return NO;
+
+    // SEL其实内部就是一个C字符串, 参见sel_alloc方法
+    const char *name = (const char *)(void *)sel;
+
+    if (sel == search_builtins(name)) return YES;
+
+    mutex_locker_t lock(selLock);
+    if (namedSelectors) {
+        return (sel == (SEL)NXMapGet(namedSelectors, name));
+    }
+    return false;
+}
+
+
+static SEL search_builtins(const char *name) 
+{
+#if SUPPORT_PREOPT
+    if (builtins) return (SEL)builtins->get(name);
+#endif
+    return nil;
+}
+
+// 注册sel, 如果方法名相同，则sel也相同
+
+static SEL __sel_registerName(const char *name, bool shouldLock, bool copy) 
+{
+    SEL result = 0;
+
+    if (shouldLock) selLock.assertUnlocked();
+    else selLock.assertLocked();
+
+    if (!name) return (SEL)0;
+    // 从预存的sels中查找
+
+    result = search_builtins(name);
+    if (result) return result;
+    
+    // 从table 中查找
+    conditional_mutex_locker_t lock(selLock, shouldLock);
+    if (namedSelectors) {
+        result = (SEL)NXMapGet(namedSelectors, name);
+    }
+    if (result) return result;
+
+    // No match. Insert.
+
+    if (!namedSelectors) {
+        namedSelectors = NXCreateMapTable(NXStrValueMapPrototype, 
+                                          (unsigned)SelrefCount);
+    }
+    if (!result) {
+        // 创建SEL并加入到map中
+        result = sel_alloc(name, copy);
+        // fixme choose a better container (hash not map for starters)
+        NXMapInsert(namedSelectors, sel_getName(result), result);
+    }
+
+    return result;
+}
+
+
+SEL sel_registerName(const char *name) {
+    return __sel_registerName(name, 1, 1);     // YES lock, YES copy
+}
+
+SEL sel_registerNameNoLock(const char *name, bool copy) {
+    return __sel_registerName(name, 0, copy);  // NO lock, maybe copy
+}
+
+
+// 2001/1/24
+// the majority of uses of this function (which used to return NULL if not found)
+// did not check for NULL, so, in fact, never return NULL
+//
+SEL sel_getUid(const char *name) {
+    return __sel_registerName(name, 2, 1);  // YES lock, YES copy
+}
+
+
+// 判断两个sel是否相等,直接比较指针即可;(typedef struct objc_selector *SEL)
+BOOL sel_isEqual(SEL lhs, SEL rhs)
+{
+    return bool(lhs == rhs);
+}
+
+
+#endif
